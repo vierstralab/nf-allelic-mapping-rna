b7732399126a692cde859ef0fd3aaf475c1bb1d7
Sergey Abramov <aswq22013@gmail.com>
2022-07-05 14:33:11 +0300

    debug

--- bin/tags_to_babachi_format.py
+++ bin/tags_to_babachi_format.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import sys
+import os
 
 from pileup_file import SNV
 
 
 def main(in_file):
+    print(os.exists(in_file))
     df = pd.read_table(in_file)
     columns = SNV.get_fields()[:6] + ['ref_counts', 'alt_counts']
     df[columns].to_csv(sys.stdout, sep='\t', header=None, index=False)
