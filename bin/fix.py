c941b9e8729fdd127c113fe86bc47a6bda97a686
wishabc <aswq22013@gmail.com>
2023-01-24 21:48:33 -0800

    script for annotation

--- bin/fix.py
+++ bin/fix.py
@@ -8,22 +8,7 @@ def alt_str_has_single(alts_str):
     return sum([(len(alt) == 1) and (alt in nucleotides) for alt in alts_str.split(',')]) > 0
 
 
-def main(snps, annotations):
-    repeated = annotations[(annotations['topmed'] != '.') &
-                           (annotations['topmed'].notna()) &
-                           (annotations['alts'].apply(alt_str_has_single)) &
-                           (annotations['ref'].isin(nucleotides))
-    ].drop_duplicates(['#chr', 'end', 'ref', 'alts', 'topmed']).value_counts(['#chr', 'start', 'end', 'ref'])
-    repeated = repeated[repeated > 1].reset_index()[['#chr', 'start', 'end', 'ref']]
-    repeated = annotations.merge(repeated, on=['#chr', 'start', 'end', 'ref'])
-
-    repeated = repeated.groupby(['#chr', 'start', 'end', 'ref'])['alts'].apply(
-        lambda alts_on_same_pos:
-        sum([alt_str_has_single(alts_str) for alts_str in alts_on_same_pos]) > 1
-    )
-
-    assert repeated.sum() == 0
-    
+def main(snps, annotations):   
     annotations = annotations[(annotations['topmed'] != '.') &
                            (annotations['topmed'].notna()) &
                            (annotations['alts'].apply(alt_str_has_single)) &
@@ -35,7 +20,7 @@ def main(snps, annotations):
     merged = snps.merge(annotations, 
         on=['#chr', 'start', 'end', 'ref'],
         how='left')
-    merged['RAF'] = merged['topmed'].apply(lambda x: '.' if pd.isna(x) or x == '.'
+    merged['RAF'] = merged['topmed'].apply(lambda x: '.' if pd.isna(x)
                                            else float(x.split(',')[0]))
     merged['AAF'] = merged.apply(
         lambda row:
@@ -43,12 +28,13 @@ def main(snps, annotations):
         dict(zip(row['alts'].split(','), row['topmed'].split(',')[1:])).get(row['alt'], '.'),
         axis=1
     )
-    return merged[['#chr', 'start', 'end', 'ID', 'ref', 'alt', 'ref_counts', 'alt_counts', 'sample_id', 'AAF', 'RAF', 'FMR']]
+    return merged[['#chr', 'start', 'end', 'ID', 'ref', 'alt',
+     'ref_counts', 'alt_counts', 'sample_id', 'AAF', 'RAF', 'FMR']]
 
 if __name__ == '__main__':
     dbsnp_annotation = pd.read_table(sys.argv[1],
         header=None, names=['#chr', 'start', 'end', 'ref', 'alts', 'topmed'])
 
-    snps_to_annotate = pd.read_table(sys.argv[2], header=None, names=['#chr', 'start', 'end', 'ref', 'alt'])
+    snps_to_annotate = pd.read_table(sys.argv[2])
     df = main(snps_to_annotate, dbsnp_annotation)
     df.to_csv(sys.argv[3], sep='\t', index=False, header=None)
